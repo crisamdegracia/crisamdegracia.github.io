96d9e6ab1807e085ee601e2246cbc375f8dc0037
Sam <crisamdegracia@gmail.com>
2017-08-18 02:47:58 +0300

    joly crap!

--- js/loading.js
+++ js/loading.js
@@ -1,22 +1,50 @@
-//$(document).ready(function(){
-//
-//    
-//    var overlay = $('#overlay');
-//    $(window).on('load',function(){
-//        overlay.css('display','none');
-//    })
-//})
+$(document).ready(function(){
 
 
-(function(){
-    
-    var overlay = document.getElementById('overlay');
+    var overlay = $('#overlay'),
+        main    = $('#main');
+    $(window).on('load',function(){
+        overlay.css('display','none');
+        main.css(
 
-    window.addEventListener('load', function(){
-        overlay.style.display = 'none';
-        
+            'display','block',
+            'position','relative',
+            'overflow-y', 'scroll',
+            'margin' , '0 0',
+            'padding' , '0 0'
+        )
+
     })
-    
-})();
+})
+
+
+//(function(){
+//
+//    //    #aboutMeText .readMoreBtn,
+//    //    #about,
+//    //    #project,
+//    //    #resources,
+//    //    #homeMain ,
+//    //    #main
+//    //the loader
+//    var overlay       = document.getElementById('overlay'),
+//        body          = document.getElementById('main'),
+//        homeMain      = document.getElementById('homeMain'),
+//        resources     = document.getElementById('resources'),
+//        about         = document.getElementById('about'),
+//        readMoreBtn   = document.getElementsByTagName('#aboutMeText .readMoreBtn')
+//
+//    window.addEventListener('load', function(){
+//        overlay.style.display = 'none'; 
+//
+//        body.style.visibility = 'visible'
+//        body.style.height = '100%'
+////        resources.style.visibility = 'visible'
+////        about.style.visibility = 'visible'
+////        readMoreBtn.style.visibility = 'visible'
+//
+//    })
+//
+//})();
 
 
