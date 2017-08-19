07475d8a8e1267b0beb3fbf74e05af040a1bd355
Sam <crisamdegracia@gmail.com>
2017-08-19 18:53:22 +0300

    fix loading

--- js/loading.js
+++ js/loading.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
     $(window).on('load',function(){
         overlay.css('display','none');
         main.css(
-            'display','block'
+         'display','block'
         )
         console.log('loads complete.')
     })
