a79d3341c524b8f987cae81ffb19670227e9d6e8
Sam <crisamdegracia@gmail.com>
2017-08-23 02:33:27 +0300

    add hire me btn

---
+++ js/floatingBtn.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    var btn = $('#floatingBtn'),
+        el  = $('#floatingBtn div')
+    
+//    btn.click(function(){
+//        
+//        el.css({
+//            width : "100px",
+//            heigth : "100px"
+//        })
+//        
+//    })
+})
--- js/shrink.js
+++ js/shrink.js
@@ -5,14 +5,17 @@ $(window).scroll(function(e){
     if($(document).scrollTop() > 15 ){ 
         $('nav').removeClass('shrinkBack');
         $('nav').addClass('shrink');
-        
+        $('#floatingBtn').css('display','block');    
+
     } 
+
     else {
         $('nav').addClass('shrinkBack');
         $('nav').removeClass('shrink');
+        $('#floatingBtn').css('display','none'); 
 
     }
-    
+
     e.preventDefault();
     e.stopPropagation();
 })
